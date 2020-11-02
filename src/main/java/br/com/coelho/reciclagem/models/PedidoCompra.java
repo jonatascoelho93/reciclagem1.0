d57a54f0562de45824eeb941bf1bf07d5eb31668
Jonatas <jonatas.coelho@aluno.faculdadeimpacta.com.br>
2020-11-01 21:33:08 -0300

    DAO criado, atualização da classe PedidoCompra

--- src/main/java/br/com/coelho/reciclagem/models/PedidoCompra.java
+++ src/main/java/br/com/coelho/reciclagem/models/PedidoCompra.java
@@ -1,6 +1,7 @@
 package br.com.coelho.reciclagem.models;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -9,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -24,5 +27,36 @@ public class PedidoCompra {
 
 	@NotNull
 	private BigDecimal totalCompra;
+
+	@Temporal(TemporalType.DATE)
+	private Calendar dataPublicacao;
+
+	public List<PedidoDeCompraItens> getPedidoDeCompraItens() {
+		return pedidoDeCompraItens;
+	}
+
+	public void setPedidoDeCompraItens(List<PedidoDeCompraItens> pedidoDeCompraItens) {
+		this.pedidoDeCompraItens = pedidoDeCompraItens;
+	}
+
+	public BigDecimal getTotalCompra() {
+		return totalCompra;
+	}
+
+	public void setTotalCompra(BigDecimal totalCompra) {
+		this.totalCompra = totalCompra;
+	}
+
+	public Calendar getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(Calendar dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+
+	public Long getIdPedidoCompra() {
+		return idPedidoCompra;
+	}
 
 }
